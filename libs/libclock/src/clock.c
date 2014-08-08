dfebcbcb1f00afb7b69d76d70e6c0f6449aaabd9
Jack Lian (Jing Wu Lian) <hljw4@hotmail.com>
2014-08-08 19:16:21 +1000

    Implement start_timer and stop_timer
    Start_timer and stop_timer register a interrupt end point
    with the kernel as well as map the clock register and
    configure them.

--- libs/libclock/src/clock.c
+++ libs/libclock/src/clock.c
@@ -2,6 +2,36 @@
 #include <string.h>
 #include <clock/clock.h>
 
+int start_timer(seL4_CPtr interrupte_ep){
+    //register handler with kernel
+    seL4_IRQHandler_SetEndpoint(interrupte_ep);
+
+    //map device
+    //if mapdevice fails, it panics
+    vaddr * epit1_cr = map_device((*paddr)0x20D_0000, 4); 
+    epit_cr = 0b000000_01_10_0_0_0_0_0_1_000000000001_1_1_0_1;
+    //vaddr * epit1_sr = map_device((*paddr)0x20D_0004, 4); 
+    vaddr * epit1_lr = map_device((*paddr)0x20D_0008, 4); 
+    epit_lr = 330;
+    vaddr * epit1_cmpr = map_device((*paddr)0x20D_000C, 4); 
+    epit_cmpr = 330;
+    //vaddr * epit1_cnr = map_device((*paddr)0x20D_0010, 4); 
+    
+    return CLOCK_R_OK;
+}
+
+int stop_timer(void){
+    //map device
+    //write value to device register
+    vaddr * epit1_cr = map_device((*paddr)0x20D_0000, 4); 
+    epit_cr = 0b000000_01_10_0_0_0_0_0_1_000000000001_1_1_0_0;
+    //unmap device?
+
+    //remove handler with kernel
+    seL4_IRQHandler_Clear(interrupte_ep);
+    
+    return CLOCK_R_OK;
+}
 
 uint32_t register_timer(uint64_t delay, timer_callback_t callback, void *data) {
     return 0; // just fail for now

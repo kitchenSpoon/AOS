80adb8ae11884a1eeebf10efdaf0b9c63f3b01fb
Jack Lian (Jing Wu Lian) <hljw4@hotmail.com>
2014-08-17 17:42:35 +1000

    Add implementation of frame_alloc
    Haven't had any test though

--- apps/sos/src/frametable.c
+++ apps/sos/src/frametable.c
@@ -3,10 +3,13 @@
 #include <cspace/cspace.h>
 #include <mapping.h>
 #include <ut_manager/ut.h>
+#include <strings.h>
 
 #define FRAME_STATUS_UNTYPED     0
 #define FRAME_STATUS_FREE        1
 #define FRAME_STATUS_ALLOCATED   2
+
+#define PAGE_SIZE (1ull<<12)
 
 /* Frame table entry structure */
 typedef struct {
@@ -24,28 +27,50 @@ int first_free;
 /* Keep track of initialisation status of the frame table */
 static bool frame_initialised;
 
-/*
- * Find index of the next free frame in the frame table
- * 
- * Returns index of the next free frame
- */
-static int
-_next_free(void) {
-    /* Check if we have any free frames */
-    /* TODO:Change this to use a linked list */
-    for(int i = 0; i < 10; i++){
-        if(frame_table[i].fte_status == FRAME_STATUS_UNTYPED) return i;
-    }
-
-    /* No Free Frames */
-    return -1;
-}
-
 
 int frame_init(){
     /* First free should not be one */
     first_free = -1;
     frame_initialised = true;
+    return 0;
+}
+
+/*
+ * Retype a page
+ */
+static void
+_alloc_n_map(const seL4_ARM_PageDirectory pd, const seL4_Word vaddr, seL4_Word *paddr, seL4_CPtr *cap) {
+    /* Allocate memory */
+    *paddr = ut_alloc(seL4_PageBits);
+
+    /* Retype memory */
+    cspace_err_t cspace_err = cspace_ut_retype_addr(*paddr, 
+                                                    seL4_ARM_SmallPageObject, 
+                                                    seL4_PageBits,
+                                                    cur_cspace, 
+                                                    cap);
+    assert(cspace_err == CSPACE_NOERROR);
+
+    /* Map memory */
+    int err = map_page(*cap, pd, vaddr, seL4_AllRights, seL4_ARM_Default_VMAttributes);
+    assert(!err);
+}
+
+int frame_alloc(seL4_Word *vaddr){
+    if(first_free == -1) return 1;
+    
+    /* Update linked list */
+    int ind = first_free;
+    first_free = frame_table[ind].fte_next_free;
+
+    *vaddr = 0x20000000 + ind * PAGE_SIZE;
+
+    _alloc_n_map(seL4_CapInitThreadPD, *vaddr,
+                &frame_table[ind].fte_paddr, &frame_table[ind].fte_cap);
+
+    /* Zero memory */
+    bzero((void *)(*vaddr), (size_t)PAGE_SIZE);
+
     return 0;
 }
 
@@ -70,6 +95,10 @@ int frame_free(int id){
     ut_free(paddr, seL4_PageBits);
     
     frame_table[id].fte_status = FRAME_STATUS_UNTYPED;
-    
+
+    /* Update linked list */
+    frame_table[id].fte_next_free = first_free;
+    first_free = id;
+
     return 0;
 }
